a9768d51648ec083d84685985b172b26cbbf51a8
Michael Myers <michaelzmyers@gmail.com>
2017-04-04 03:22:34 -0700

    testing 3

--- server/config.js
+++
@@ -1,8 +0,0 @@
-// this normally wouldn't be uploaded to the repo
-// but it's just for demonstration
-// testing
-
-module.exports = {
-  secret: '99j9j9jdjei3DFEC',
-  mongodb_uri: 'mongodb://michaelzmyers:minarik1@ds145800.mlab.com:45800/aetek-secundus'
-}
